0ba4e8b58221cfd78069cde44241574df27f547a
likaifeng <1980942486@qq.com>
2018-12-01 11:35:11 +0800

    添加用户布局完成

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -23,6 +23,7 @@
       <el-button
         type="success"
         plain
+        @click="dialogFormVisible=true"
       >添加用户</el-button>
     </div>
     <!-- 用户列表 -->
@@ -73,24 +74,45 @@
           </el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
-              <el-button
-                type="primary"
-                plain
-                class="el-icon-edit"
-                @click="handleEdit(scope.$index, scope.row)"
-              ></el-button>
-              <el-button
-                plain
-                type="danger"
-                class="el-icon-delete"
-                @click="handleDelete(scope.$index, scope.row)"
-              ></el-button>
-              <el-button
-                plain
-                type="success"
-                class="el-icon-check"
-                @click="handleDelete(scope.$index, scope.row)"
-              ></el-button>
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="编辑"
+                placement="top"
+              >
+                <el-button
+                  type="primary"
+                  plain
+                  class="el-icon-edit"
+                  @click="handleEdit(scope.$index, scope.row)"
+                ></el-button>
+              </el-tooltip>
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="删除"
+                placement="top"
+              >
+                <el-button
+                  plain
+                  type="danger"
+                  class="el-icon-delete"
+                  @click="handleImpower(scope.$index, scope.row)"
+                ></el-button>
+              </el-tooltip>
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="授权"
+                placement="top"
+              >
+                <el-button
+                  plain
+                  type="success"
+                  class="el-icon-check"
+                  @click="handleDelete(scope.$index, scope.row)"
+                ></el-button>
+              </el-tooltip>
             </template>
           </el-table-column>
         </el-table>
@@ -108,6 +130,53 @@
       :total="total"
     >
     </el-pagination>
+    <!-- 添加用户 -->
+    <div>
+      <el-dialog
+        title="添加用户"
+        :visible.sync="dialogFormVisible"
+      >
+        <el-form
+          :model="form"
+          :label-width="formLabelWidth"
+        >
+          <el-form-item label="用户名">
+            <el-input
+              v-model="form.name"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input
+              v-model="form.name"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input
+              v-model="form.name"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="电话">
+            <el-input
+              v-model="form.name"
+              autocomplete="off"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div
+          slot="footer"
+          class="dialog-footer"
+        >
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button
+            type="primary"
+            @click="dialogFormVisible = false"
+          >确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 <script>
@@ -123,7 +192,20 @@ export default {
         query: '',
         pagenum: 1,
         pagesize: 1
-      }
+      },
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '120px'
     }
   },
   mounted () {
@@ -140,13 +222,13 @@ export default {
       })
     },
     handleSizeChange (val) {
-      console.log(`每页 ${val} 条`)
+      // console.log(`每页 ${val} 条`)
       this.params.pagesize = val
       console.log(this.params)
       this.init()
     },
     handleCurrentChange (val) {
-      console.log(`当前页: ${val}`)
+      // console.log(`当前页: ${val}`)
       this.params.pagenum = val
       console.log(this.params)
       this.init()
