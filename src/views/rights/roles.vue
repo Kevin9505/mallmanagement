0eab691cdd327e49c26e885bc7ec1c3bcf2f5388
likaifeng <1980942486@qq.com>
2018-12-03 19:39:40 +0800

    角色列表页面布局完成

--- src/views/rights/roles.vue
+++ src/views/rights/roles.vue
@@ -6,5 +6,157 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 添加角色 -->
+    <div style="margin-top: 15px;">
+      <el-button
+        type="success"
+        plain
+        @click="rolesdialogFormVisible=true"
+      >添加角色</el-button>
+    </div>
+    <!-- 角色信息展示列表 -->
+    <template>
+      <el-table
+        :data="tableData5"
+        border
+        style="width: 100%; margin-top: 15px;"
+      >
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form
+              label-position="left"
+              inline
+              class="demo-table-expand"
+            >
+              <el-form-item label="商品名称">
+                <span>{{ props.row.name }}</span>
+              </el-form-item>
+              <el-form-item label="所属店铺">
+                <span>{{ props.row.shop }}</span>
+              </el-form-item>
+              <el-form-item label="商品 ID">
+                <span>{{ props.row.id }}</span>
+              </el-form-item>
+              <el-form-item label="店铺 ID">
+                <span>{{ props.row.shopId }}</span>
+              </el-form-item>
+              <el-form-item label="商品分类">
+                <span>{{ props.row.category }}</span>
+              </el-form-item>
+              <el-form-item label="店铺地址">
+                <span>{{ props.row.address }}</span>
+              </el-form-item>
+              <el-form-item label="商品描述">
+                <span>{{ props.row.desc }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+          type="index"
+          width="50"
+        >
+        </el-table-column>
+        <el-table-column
+          label="角色名称"
+          prop="id"
+        >
+        </el-table-column>
+        <el-table-column
+          label="描述"
+          prop="name"
+        >
+        </el-table-column>
+        <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="编辑"
+                placement="top"
+              >
+                <el-button
+                  type="primary"
+                  plain
+                  class="el-icon-edit"
+                  @click="handleEdit(scope.row)"
+                ></el-button>
+              </el-tooltip>
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="删除"
+                placement="top"
+              >
+                <el-button
+                  plain
+                  type="danger"
+                  class="el-icon-delete"
+                  @click="handleDelete(scope.row.id)"
+                ></el-button>
+              </el-tooltip>
+              <el-tooltip
+                class="item"
+                effect="dark"
+                content="授权角色"
+                placement="top"
+              >
+                <el-button
+                  plain
+                  type="success"
+                  class="el-icon-check"
+                  @click="handleImpower(scope.row)"
+                ></el-button>
+              </el-tooltip>
+            </template>
+          </el-table-column>
+      </el-table>
+    </template>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      rolesdialogFormVisible: false,
+      tableData5: [{
+        id: '12987122',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987125',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987126',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
